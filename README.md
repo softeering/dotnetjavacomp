2f6a1b2ff82072ddf75dd12e4e1e5c5dc12e91ed
David KOMAR <dkomar@expedia.com>
2017-12-01 17:32:13 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dotnetjavacomp
+
+dotnet and java basic web applications showing differences between ASP.NET Core and SpringBoot.
+Urls exsposed:
+- http://localhost:8080/api/static
+- http://localhost:8080/api/static
+
+## dotnet
+
+
+## java
+
---
+++ dotnet/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet.Controllers
+{
+    [Route("api")]
+    public class TestController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("static")]
+        public FileResult GetStaticFile()
+        {
+            return base.File("~/staticfile.txt", "text/plain");
+        }
+    }
+}
